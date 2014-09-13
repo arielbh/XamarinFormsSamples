5e9942bc74375a84440cead9a7ebfc5843d43363
Ariel Ben Horesh <ariel_bh@yahoo.com>
2014-09-14 00:31:36 +0300

    Initial solution with updated packages.

---
+++ LayoutDemo/LayoutDemo.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace LayoutDemo.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = LayoutDemo.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
