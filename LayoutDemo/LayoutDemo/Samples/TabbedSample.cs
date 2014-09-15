2ceb71104726caf9f690e02b3b1f703af71646d1
Ariel Ben Horesh <ariel_bh@yahoo.com>
2014-09-15 08:16:11 +0300

    Tabbed Sample

---
+++ LayoutDemo/LayoutDemo/Samples/TabbedSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutDemo.Samples
+{
+    class TabbedSample : TabbedPage
+    {
+        public TabbedSample()
+        {
+            Title = "Tabs Sample";
+            Children.Add(new Page1());
+            Children.Add(new Page2());
+        }
+    }
+}
